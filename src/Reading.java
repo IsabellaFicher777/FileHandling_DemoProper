16685078d9dd8711c4a5b286b53432952be49278
IsabellaFicher777 <ficheril@oldcolony.info>
2026-04-13 09:47:27 -0400

    printed out the fellowship

--- src/Reading.java
+++ src/Reading.java
@@ -7,6 +7,7 @@ public class Reading
     {
         File myFile = new File("fellowship.txt");
 
+        // Check to ensure the file does exit
         if(!myFile.exists())
         {
             System.out.println("File does not exist");
@@ -14,5 +15,20 @@ public class Reading
         }
 
         Scanner inputFile = new Scanner(myFile);
+
+        System.out.println("Members of the Fellowship:");
+        System.out.println("==========================");
+
+        int count = 0;
+
+        while(inputFile.hasNextLine())
+        {
+            count++;
+            inputFile.nextLine();
+        }
+
+        inputFile.close();
+
+        System.out.println("Total members: " + count);
     }
 }
